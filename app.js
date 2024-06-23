071f18eda0486260c7ca27ce61ca249169d59faa
Said Masih <saidmsajady@gmail.com>
2024-06-23 14:12:10 -0700

    Removed the html files for ejs and have the base foundation for the application

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ const express = require('express');
 // Express App
 const app = express();
 
+// Register View Engiene
+app.set('view engine', 'ejs');
+
 // Listen for Request
 app.listen(3000);
 
 app.get('/', (req, res) => {
     //res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname});
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
     //res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', { root: __dirname});
+    res.render('about');
 });
+
+app.get('/charcter/create', (req, res) => {
+    res.render('create');
+})
 
 // Redirects
 app.get('/about-us', (req, res) => {
@@ -24,5 +31,5 @@ app.get('/about-us', (req, res) => {
 // 404 page
 // Middlewear
 app.use((req, res) => {
-    res.sendFile('./views/404.html', { root: __dirname});
+    res.status(404).render('404')
 })
