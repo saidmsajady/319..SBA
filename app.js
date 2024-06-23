6601880fbb70449c1acfdf4938d5c1a488752130
Said Masih <saidmsajady@gmail.com>
2024-06-23 13:32:01 -0700

    Had to create new git repo

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+// Express App
+const app = express();
+
+// Listen for Request
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    //res.send('<p>home page</p>');
+    res.sendFile('./views/index.html', { root: __dirname});
+});
+
+app.get('/about', (req, res) => {
+    //res.send('<p>about page</p>');
+    res.sendFile('./views/about.html', { root: __dirname});
+});
+
+// Redirects
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+})
+
+// 404 page
+// Middlewear
+app.use((req, res) => {
+    res.sendFile('./views/404.html', { root: __dirname});
+})
