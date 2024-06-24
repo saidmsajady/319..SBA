d3192cffdeea82c1d74a1c3fbf7d5c8a402b86e8
Said Masih <saidmsajady@gmail.com>
2024-06-23 19:59:45 -0700

    Connected Mongoose

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 // Express App
 const app = express();
 
+// Connect to MongoDB
+const dbURI = 'mongodb+srv://saidmsajady:Y1zQShpTvxQr4fsq@mongocluster.2h7ovds.mongodb.net/dc-charcters?retryWrites=true&w=majority&appName=MongoCluster';
+
+mongoose.connect(dbURI)
+    .then((result) => app.listen(3000)) // Listen for Request
+    .catch((err) => console.log(err))
+
 // Register View Engiene
 app.set('view engine', 'ejs');
-
-// Listen for Request
-app.listen(3000);
 
 // Static Built-in Middlewear for Style Sheet
 app.use(express.static('styles'));
